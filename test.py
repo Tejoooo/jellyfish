0b828dc29a8c17b24a743da50cc997e49f7928f5
ravikumargrk <ravikumargrk@hotmail.com>
2025-08-16 19:40:56 +0000

    +- cleanup

--- test.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description='A text editor')
-parser.add_argument('file', help='filepath to edit.', default='untitled.txt', nargs='?')
-args = parser.parse_args()
-
-print(vars(args))
